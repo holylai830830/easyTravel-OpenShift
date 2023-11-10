0ccc31a4294305b8619a1954137e5f5890327dcd
holylai830830 <34336275+holylai830830@users.noreply.github.com>
2023-11-10 16:44:48 +0800

    Delete clean.sh

--- clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for name in 'easytravel-mongodb' 'easytravel-backend' 'easytravel-frontend' 'easytravel-www' 'easytravel-loadgen'; do
-  oc delete svc ${name}
-  oc delete rc ${name}
-  oc delete pod ${name}
-done
